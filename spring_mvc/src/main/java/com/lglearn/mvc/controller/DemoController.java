44511c354e4f8a4ca078d916c60a1efbe3d8772e
Chen Hao <287329409@qq.com>
2021-02-24 21:47:11 +0800

    基础MVC

--- spring_mvc/src/main/java/com/lglearn/mvc/controller/DemoController.java
+++ spring_mvc/src/main/java/com/lglearn/mvc/controller/DemoController.java
@@ -1,17 +1,24 @@
 package com.lglearn.mvc.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.util.Date;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/demo")
 public class DemoController {
 
+    //对原生servlet api的支持
     @RequestMapping("/handle01")
-    public ModelAndView handle01(){
+    public ModelAndView handle01(HttpServletRequest request, HttpServletResponse response, HttpSession session) {
         Date date = new Date();
         //封装数据和页面信息的ModelAndView
         ModelAndView modelAndView = new ModelAndView();
@@ -21,4 +28,24 @@ public class DemoController {
         return modelAndView;
     }
 
+    @RequestMapping("/handle02")
+    public String handle02(ModelMap modelMap) {
+        Date date = new Date();
+        modelMap.addAttribute("date", date);
+        return "success";
+    }
+
+    @RequestMapping("/handle03")
+    public String handle03(Model model) {
+        Date date = new Date();
+        model.addAttribute("date", date);
+        return "success";
+    }
+
+    @RequestMapping("/handle04")
+    public String handle04(Map<String, Object> map) {
+        Date date = new Date();
+        map.put("date", date);
+        return "success";
+    }
 }
---
+++ spring_mvc/src/main/java/com/lglearn/mvc/pojo/User.java
@@ -0,0 +1,12 @@
+package com.lglearn.mvc.pojo;
+
+import lombok.Data;
+
+@Data
+public class User {
+
+    private String id;
+
+    private String username;
+
+}
